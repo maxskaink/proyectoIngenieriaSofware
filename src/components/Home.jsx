ec0142e7356b940d0a033de17e4078aaa25b53f3
santiago1scan <93345838+santiago1scan@users.noreply.github.com>
2024-05-14 18:01:01 -0500

    se corrige sideBar con fondo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,12 @@
-import { AddMoney } from "./AddMoney";
+
 import "../styles/home.css";
 
 /* Es el componente principal que debe tener acceso al resto de funciones en la aplicacion */
 export const Home = () => {
   return (
-    <div>
+    <div className= "contenedor-menu" >
       <h1>Bienvenido</h1>
-      <p>No se han encontrado anomalidades en el inventario</p>
-      <p>Este es el home</p>
-      <AddMoney />
+
     </div>
   );
 };
