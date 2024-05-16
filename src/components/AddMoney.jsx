a9e3b2636dd0f493871e0435074d65230b48e92a
santiago1scan <93345838+santiago1scan@users.noreply.github.com>
2024-05-16 14:08:10 -0500

    presentacion

--- src/components/AddMoney.jsx
+++ src/components/AddMoney.jsx
@@ -1,5 +1,6 @@
 import  { useState } from 'react';
 import {addMoney} from '../helpers/querys';
+import '../styles/addMoney.css';
 
 export const AddMoney = () => {
     const [amount, setAmount] = useState('');
@@ -26,17 +27,19 @@ export const AddMoney = () => {
     };
 
     return (
-        <div>
-            <h2>Agregar Dinero</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={amount}
-                    onChange={handleInputChange}
-                    placeholder="Ingrese la cantidad"
-                />
-                <button type="submit">Agregar</button>
-            </form>
+        <div className='container'>
+            <div className='containerAddMoney'>
+                <h2>Agregar Dinero</h2>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={amount}
+                        onChange={handleInputChange}
+                        placeholder="Ingrese la cantidad"
+                    />
+                    <button type="submit">Agregar</button>
+                </form>
+            </div>
         </div>
     );
 };
