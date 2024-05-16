74d81ae46f37f4ebbe54dad9aec87d9bfa55b5bb
Miguel Angel Calambas <67810439+maxskaink@users.noreply.github.com>
2024-05-15 19:01:57 -0500

    Se ha agregado un paquete a la BD:
    - Se ha agregado un paquete con varias funciones y procdimientos
    - Se ha creado un trigger para producto venta, para que calcule el descuento automaticamente

--- DB/paquetes.sql
+++ DB/paquetes.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE PACKAGE paquete_gestionContable AS
   FUNCTION descuentoCliente(p_clienteId CLIENTE.CEDULACLIENTE%type)
     RETURN number;
   --Procedimiento que reciba como parámetro el porcentaje que se le quiere subir o bajar el precio a todos los productos de una sucursal.
-  PROCEDURE cambiarPreciosTodo(p_cambio number);
+  PROCEDURE cambiarPreciosTodo(p_porcentajeCambio number);
 
     -- Crear un tipo de registro para almacenar los detalles de una compra
   TYPE compra_detalle IS RECORD (
@@ -36,15 +36,15 @@ CREATE OR REPLACE PACKAGE paquete_gestionContable AS
   -- Función para identificar a los principales clientes basados en el monto total gastado en el último mes
   FUNCTION principales_clientes_ultimo_mes RETURN clientes_tabla;
 
-end;
-
+END paquete_gestionContable;
+/
 
 CREATE OR REPLACE PACKAGE BODY paquete_gestionContable AS
   --Función que reciba como parámetro el id del cliente y devuelva el porcentaje de descuento total que se le puede hacer por fidelización
   FUNCTION descuentoCliente(p_clienteId CLIENTE.CEDULACLIENTE%type)
     RETURN number
   IS
-    v_fechaNacimiento number;
+    v_fechaNacimiento date;
   BEGIN
     select FECHANACIMIENTO 
     into v_fechaNacimiento
@@ -56,16 +56,16 @@ CREATE OR REPLACE PACKAGE BODY paquete_gestionContable AS
     ELSE 
       RETURN  0;
     END IF;
-  END;
+  END descuentoCliente;
     
   --Procedimiento que reciba como parámetro el porcentaje que se le quiere subir o bajar el precio a todos los productos de una sucursal.
   PROCEDURE cambiarPreciosTodo(p_porcentajeCambio number) IS
   BEGIN 
     UPDATE Producto  set PrecioActual = PrecioActual*(1-p_porcentajeCambio);
-  END;
+  END cambiarPreciosTodo;
 
   -- Procedimiento para generar un historial de compras para un cliente específico
-    PROCEDURE historial_compras_cliente (
+  PROCEDURE historial_compras_cliente (
     p_cedulaCliente IN CLIENTE.CEDULACLIENTE%TYPE,
     p_historial OUT compras_tabla
   ) AS
@@ -99,7 +99,7 @@ CREATE OR REPLACE PACKAGE BODY paquete_gestionContable AS
     p_historial := v_historial;
   END historial_compras_cliente;
 
-    FUNCTION principales_clientes_ultimo_mes RETURN clientes_tabla AS
+  FUNCTION principales_clientes_ultimo_mes RETURN clientes_tabla AS
     v_clientes clientes_tabla;
     v_index PLS_INTEGER := 0;
   BEGIN
@@ -129,25 +129,6 @@ CREATE OR REPLACE PACKAGE BODY paquete_gestionContable AS
     RETURN v_clientes;
   END principales_clientes_ultimo_mes;
   
-END;
+END paquete_gestionContable;
 
 --Hacer triger de pedido para calcular el descuento si la compra se realizo en el cumple del cliente
-CREATE OR REPLACE TRIGGER prom_productoVenta
-AFTER INSERT ON productoventa 
-FOR EACH ROW 
-DECLARE
-  v_sumaProductos number;
-  v_idCliente CLIENTE.CEDULACLIENTE%type;
-BEGIN
-  SELECT SUM(preciounitario)
-  INTO v_sumaProductos
-  FROM productoventa 
-  WHERE codigoventa = :NEW.codigoventa;
-  
-  SELECT CEDULACLIENTE 
-  INTO v_idCliente
-  FROM PEDIDO
-  WHERE :NEW.codigoventa;
-  
-  UPDATE PEDIDO SET PRECIOTOTAL = v_sumaProductos(1- paquete_gestionContable.descuentoCliente(v_idCliente));
-END;
